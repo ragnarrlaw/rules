8338602fa9d088a2c1518c1b98e3b5ad4690c059
BhathiyaVandebona <malingabhathiya@gmail.com>
2024-12-10 10:14:42 +0530

    add test case after segment fault fix

--- rule_engine/evaluator/evaluator_test.go
+++ rule_engine/evaluator/evaluator_test.go
@@ -36,17 +36,19 @@ func TestEvaluate(t *testing.T) {
 				CartPrice:               0.0,
 				CartPriceAfterDiscounts: 80.0,
 				Discounts: map[string]string{
+					"DISCOUNT30": "min_cart_price > 500 then cart_percentage 8",
 					"DISCOUNT10": "product_id in [1, 2] and category_id = 2 then product_percentage 10",
-					// "DISCOUNT30": "min_cart_price > 500 then cart_percentage 8",
 				},
 			},
 		},
 	)
 	Evaluate(sampleContext)
+
+	if (((*sampleContext).Stores)[0].Products)[0].DiscountedPrice != 90.0 {
+		t.Errorf("Expected discounted price for product 1 to be 90.0, but got %f", (((*sampleContext).Stores)[0].Products)[0].DiscountedPrice)
+	}
+
 	if ((*sampleContext).Stores)[0].CartPriceAfterDiscounts != 80.0 {
 		t.Errorf("Expected cart price after discounts to be 80.0, but got %f", ((*sampleContext).Stores)[0].CartPriceAfterDiscounts)
 	}
-	// if (((*sampleContext).Stores)[0].Products)[0].DiscountedPrice != 90.0 {
-	// 	t.Errorf("Expected discounted price for product 1 to be 90.0, but got %f", (((*sampleContext).Stores)[0].Products)[0].DiscountedPrice)
-	// }
 }
